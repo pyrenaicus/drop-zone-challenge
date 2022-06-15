c68eb859b87d15a43f5cd3a02d905ff27ff5520b
pyrenaicus <pauguerrero@gmail.com>
2022-06-15 18:02:48 +0200

    login-logout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
+import { useReducer } from "react";
 import logo from "./logo.svg";
 import { Router, Route, Switch } from "react-router-dom";
 
 import { useAuth0 } from "@auth0/auth0-react";
 
 import LoginLogout from "./components/LoginLogout";
+import DragAndDrop from "./components/DragAndDrop";
 
 // styles
 import "./App.css";
 
 function App() {
   const { isAuthenticated, isLoading, error } = useAuth0();
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "SET_DROP_DEPTH":
+        return { ...state, dropDepth: action.dropDepth };
+      case "SET_IN_DROP_ZONE":
+        return { ...state, inDropZone: action.inDropZone };
+      case "ADD_FILE_TO_LIST":
+        return { ...state, fileList: state.fileList.concat(action.files) };
+      default:
+        return state;
+    }
+  };
+  const [data, dispatch] = useReducer(reducer, {
+    dropDepth: 0,
+    inDropZone: false,
+    fileList: [],
+  });
+
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isLoading) {
+    return <h1>LOading...</h1>;
+  }
 
   const dropZone = (
     <div class="drop-zone-area">
@@ -26,17 +54,11 @@ function App() {
     </div>
   );
 
-  if (error) {
-    return <div>Oops... {error.message}</div>;
-  }
-
-  if (isLoading) {
-    return <h1>LOading...</h1>;
-  }
-
   return (
     <div className="container">
-      {isAuthenticated ? dropZone : null}
+      {/* {isAuthenticated ? dropZone : null} */}
+      {isAuthenticated ? <DragAndDrop data={data} dispatch={dispatch} /> : null}
+
       <LoginLogout />
     </div>
   );
---
+++ src/components/DragAndDrop.js
@@ -0,0 +1,60 @@
+const DragAndDrop = (props) => {
+  const { data, dispatch } = props;
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dispatch({ type: "SET_DROP_DEPTH", dropDepth: data.dropDepth + 1 });
+  };
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    dispatch({ type: "SET_DROP_DEPTH", dropDepth: data.dropDepth - 1 });
+    if (data.dropDepth > 0) return;
+    dispatch({ type: "SET_IN_DROP_ZONE", inDropZone: false });
+  };
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.dataTransfer.dropEffect = "copy";
+    dispatch({ type: "SET_IN_DROP_ZONE", inDropZone: true });
+  };
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    let files = [...e.dataTransfer.files];
+
+    if (files && files.length > 0) {
+      const existingFiles = data.fileList.map((f) => f.name);
+      files = files.filter((f) => !existingFiles.includes(f.name));
+
+      dispatch({ type: "ADD_FILE_TO_LIST", files });
+      e.dataTransfer.clearData();
+      dispatch({ type: "SET_DROP_DEPTH", dropDepth: 0 });
+      dispatch({ type: "SET_IN_DROP_ZONE", inDropZone: false });
+    }
+  };
+
+  return (
+    <div className="drop-zone-area">
+      <div
+        className={
+          data.inDropZone
+            ? "drop-zone-target inside-drag-area"
+            : "drop-zone-target"
+        }
+        onDrop={(e) => handleDrop(e)}
+        onDragOver={(e) => handleDragOver(e)}
+        onDragEnter={(e) => handleDragEnter(e)}
+        onDragLeave={(e) => handleDragLeave(e)}
+      >
+        <p>arrastra tus archivos aquí</p>
+      </div>
+      <button class="upload-btn" onclick="upload()">
+        Subir archivos
+      </button>
+    </div>
+  );
+};
+
+export default DragAndDrop;
